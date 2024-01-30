27b39115b8113cc5d0087177626d4829939da205
nawzaysfinah <nawzaysfinah@gmail.com>
2024-01-30 12:12:59 +0800

    adjusted zoom & updated links

--- src/app/GoogleMap.js
+++ src/app/GoogleMap.js
@@ -6,9 +6,16 @@ import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
 // Your component function (note the uppercase name)
 const MyMap = () => {
+  // map = new google.maps.Map(document.getElementById("map"), {
+  //   center: { lat: 1.3521, lng: 103.8198 },
+  //   zoom: 8,
+  //   mapId: process.env.MAP_ID,
+  // });
+
   const containerStyle = {
-    width: "80vh",
+    width: "70vh",
     height: "40vh",
+    borderRadius: "10px",
   };
 
   // Memoize the center object to avoid unnecessary re-creations
@@ -29,16 +36,10 @@ const MyMap = () => {
 
   const [map, setMap] = React.useState(null);
 
-  const onLoad = React.useCallback(
-    function callback(map) {
-      // This is just an example of getting and using the map instance!!! don't just blindly copy!
-      const bounds = new window.google.maps.LatLngBounds(center);
-      map.fitBounds(bounds);
-
-      setMap(map);
-    },
-    [center]
-  ); // Add center to the dependency array
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    setMap(map);
+  }, []); // No dependencies needed here
 
   const onUnmount = React.useCallback(function callback(map) {
     setMap(null);
@@ -46,9 +47,19 @@ const MyMap = () => {
 
   const mapOptions = useMemo(() => {
     if (typeof window !== "undefined" && process.env.MAP_ID) {
+      // Replace the sample style with your actual custom map style
+      const customMapStyle = [
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [{ color: "#e9e9e9" }, { lightness: 17 }],
+        },
+        // Add more style settings as needed
+      ];
+
       // Use the StyledMapType to apply custom map styling
       const styledMapType = new window.google.maps.StyledMapType(
-        [], // Your custom map style array
+        customMapStyle,
         { name: "Styled Map" }
       );
 
@@ -56,6 +67,8 @@ const MyMap = () => {
         mapTypeControlOptions: {
           mapTypeIds: ["roadmap", "satellite", "styled_map"],
         },
+        mapTypeId: "styled_map",
+        mapTypeControl: false,
       };
     }
     return {};
@@ -65,7 +78,7 @@ const MyMap = () => {
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
-      zoom={8}
+      zoom={11.5}
       onLoad={onLoad}
       onUnmount={onUnmount}
       options={mapOptions}
@@ -78,4 +91,4 @@ const MyMap = () => {
   );
 };
 
-export default MyMap; // Export the component with the correct name
+export default MyMap;
--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,15 @@ export default function Home() {
   return (
     <div>
       <main className="flex min-h-screen flex-col items-center p-24">
-        <h1 className="font-mono text-sm">Running Club So Good 🏃🏽‍♀️🏃🏻🏃🏿</h1>
+        <h1 className="font-mono text-2xl text-green-500 text-center">
+          Running Club So Good <br></br> 🏃🏽‍♀️🏃🏻🏃🏿
+        </h1>
         <br></br>
         <MyMap />
         <br></br>
 
         <div>
-          <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+          <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
             <a
               href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
               className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
