cc93facb322c71063e8480f9428ab5665399e245
Syaz <nawzaysfinah@gmail.com>
2024-09-25 20:22:52 +0800

    updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ["@googlemaps/js-api-loader"],
+  // Add other configurations here as needed
+};
 
 export default nextConfig;
-
-module.exports = {
-  transpilePackages: ["@googlemaps/js-api-loader"],
-  // ... other configurations
-};
